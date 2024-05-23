15e3a5afcc1438d8a98ba037464ee2b87673a8e4
seojeongHHHui <sjhui0523@gmail.com>
2024-05-23 11:45:58 +0900

    guestbook2 #finish

---
+++ guestbook2/src/main/java/guestbook/controller/GuestbookServlet.java
@@ -0,0 +1,68 @@
+package guestbook.controller;
+
+import java.io.IOException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import guestbook.dao.GuestbookDao;
+import guestbook.vo.GuestbookVo;
+
+public class GuestbookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String action = request.getParameter("a");
+		if("add".equals(action)) {
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String contents = request.getParameter("contents");
+			Date now = new Date();
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			String regDate = dateFormat.format(now);
+			
+			GuestbookVo vo = new GuestbookVo();
+			vo.setName(name);
+			vo.setPassword(password);
+			vo.setContents(contents);
+			vo.setRegDate(regDate);
+			
+			new GuestbookDao().insert(vo);
+			
+			response.sendRedirect(request.getContextPath() + "/gb");
+			
+		} else if("deleteform".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/deleteform.jsp"); // webapp 기준
+			rd.forward(request, response);
+			
+		} else if("delete".equals(action)) {
+			String stringNo = request.getParameter("no");
+			Long no = Long.parseLong(stringNo);
+			String password = request.getParameter("password");
+			
+			new GuestbookDao().deleteByNoAndPassword(no, password);
+			
+			response.sendRedirect(request.getContextPath() + "/gb");
+			
+		} else {
+			/* default action (list) */
+			List<GuestbookVo> list = new GuestbookDao().findAll();
+			request.setAttribute("list", list);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+			rd.forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ guestbook2/src/main/webapp/WEB-INF/views/delete.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import="guestbook.dao.GuestbookDao" %>
+<%@ page import="guestbook.vo.GuestbookVo" %>
+<%
+	request.setCharacterEncoding("utf-8");
+	
+	String stringNo = request.getParameter("no");
+	Long no = Long.parseLong(stringNo);
+	String password = request.getParameter("password");
+	
+	new GuestbookDao().deleteByNoAndPassword(no, password);
+	
+	response.sendRedirect(request.getContextPath() + "/gb");
+%>
